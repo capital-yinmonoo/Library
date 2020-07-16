862551c4c29a768cee0f04d55feb80991fc8ab78
capital-yinmonoo <60595809+capital-yinmonoo@users.noreply.github.com>
2020-07-16 15:08:33 +0630

    by amon

---
+++ LibraryModel/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryModel
+{
+    public class RegisterModel
+    {
+        public string MemberID { get; set; }
+        public string MemberName { get; set; }
+        public string Password { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+    }
+}

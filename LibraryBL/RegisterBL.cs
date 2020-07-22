30d43f77ce55f5a8816477c42bbc379e166591d5
ALH <ckmarcheno27@gmail.com>
2020-07-22 17:38:41 +0630

    by amon

--- LibraryBL/RegisterBL.cs
+++ LibraryBL/RegisterBL.cs
@@ -6,11 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LibraryDL;
+using LibraryModel;
+
+
 
 namespace LibraryBL
 {
     public class RegisterBL
     {
+        
 
         public DataTable GetMember()
         {
@@ -18,5 +22,31 @@ namespace LibraryBL
             SqlParameter[] prms = new SqlParameter[0];
             return bdl.SelectData("M_Member_SelecAll", prms);
         }
+
+        public string Member_Save(RegisterModel rm)
+        {
+
+            LibraryEntities3 lbe = new LibraryEntities3();
+            string msg = string.Empty;
+            M_Member mb = new M_Member();
+            mb.MemberID = rm.MemberID;
+            mb.MemberName = rm.MemberName;
+            mb.MemberPassword = rm.MemberPassword;
+            mb.ContactNo = rm.ContactNo;
+            mb.Address = rm.Address;
+
+            try
+            {
+                lbe.M_Member.Add(mb);
+                lbe.SaveChanges();
+                msg = "OK";
+            }
+            catch (Exception ex)
+            {
+                msg = ex.ToString();
+            }
+            return msg;
+        }
+       
     }
 }
